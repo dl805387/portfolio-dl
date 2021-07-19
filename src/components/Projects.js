c6a87272cefcb72724fcf534ad82f5a0f737ddf0
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-07-19 17:24:23 -0400

    project description styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ import encryptor_decrypt from '../pictures/encryptor_decrypt.png'
 function Projects() {
 
     return (
-        <div>
+        <div className="projectSection">
             <h1 className="projectTitle">Projects</h1>
 
             <div className="project1">
@@ -45,12 +45,13 @@ function Projects() {
                 </div>
 
                 <div className="projDesc">
-                    <h1 className="projectSubtitle">myKdrama</h1>
+                    <h1>MY K-DRAMA</h1>
+                    <p>React.js, Express.js, Node.js, MySQL</p>
                     <ul>
                         <li>Full stack website where users can look for K-dramas and keep track of K-dramas that they have watched or will watch later</li>
                         <li>Displays the current popular K-dramas using The Movie Database API's TV Discover</li>
                         <li>Users can search for over 1,000 K-dramas</li>
-                        <li>Each K-drama can be viewed to obtain more detailed information such as the synopsis and also a list of recommended K-dramas</li>
+                        <li>Each K-drama can be viewed to obtain more detailed information such as the synopsis and a list of recommended K-dramas</li>
                         <li>User account and K-dramas are stored in a MySQL database</li>
                     </ul>
                 </div>
@@ -81,11 +82,11 @@ function Projects() {
                 </div>
 
                 <div className="projDesc">
-                    <h1 className="projectSubtitle">2048</h1>
+                    <h1>2048</h1>
+                    <p>React.js</p>
                     <ul>
                         <li>This is a clone of the 2048 game</li>
                         <li>Users must use arrow keys to move the tiles. Two tiles with the same number will merge into one. Every move will generate a random tile on the board. The goal is to make a tile with sum of 2048.</li>
-                        <li>Game code is built with Javascript</li>
                     </ul> 
                 </div>
             </div>
@@ -115,7 +116,8 @@ function Projects() {
                 </div>
 
                 <div className="projDesc">
-                    <h1 className="projectSubtitle">Encryptor</h1>
+                    <h1>ENCRYPTOR</h1>
+                    <p>Python, Pandas, Tkinter</p>
                     <ul>
                         <li>This program allows the user to encrypt and decrypt passwords/messages</li>
                         <li>Encryption is done using a combination of UTF-8 character encoding and caesar cipher</li>
@@ -150,7 +152,8 @@ function Projects() {
                 </div>
 
                 <div className="projDesc">
-                    <h1 className="projectSubtitle">Workout Journal</h1>
+                    <h1>WORKOUT JOURNAL</h1>
+                    <p>React.js, Express.js, Node.js, MySQL</p>
                     <ul>
                         <li>Full stack CRUD web app that allows users to log their workouts to keep track of exercise progression</li>
                         <li>Keeps a log of past workouts, where users can update and delete each workout.</li>
