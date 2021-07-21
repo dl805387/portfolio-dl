b859e31913553634583dbdef238357cafd4a12ac
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-07-21 03:10:35 -0400

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,22 @@ function Footer() {
 
     return (
         <div className="footer">
-            <a href="https://github.com/dl805387" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} size="5x" className="iconWhite"/></a>
-            <a href="https://www.linkedin.com/in/danny-lin-7a2094197/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} size="5x" className="iconWhite"/></a>
-            <a href="https://github.com/dl805387" target="_blank"><FontAwesomeIcon icon="file" size="5x" className="iconWhite file"/></a>
-            <a href="https://github.com/dl805387" target="_blank"><FontAwesomeIcon icon="envelope" size="5x" className="iconWhite"/></a>
+            <div className="iconPlusLabel">
+                <a href="https://github.com/dl805387" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} size="2x" className="iconWhite"/></a>
+                <a href="https://github.com/dl805387" target="_blank"><p>Github</p></a>
+            </div>
+            <div className="iconPlusLabel">
+                <a href="https://www.linkedin.com/in/danny-lin-7a2094197/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" className="iconWhite"/></a>
+                <a href="https://www.linkedin.com/in/danny-lin-7a2094197/" target="_blank"><p>LinkedIn</p></a>
+            </div>
+            <div className="iconPlusLabel">
+                <a href="https://github.com/dl805387" target="_blank"><FontAwesomeIcon icon="file" size="2x" className="iconWhite"/></a>
+                <a href="https://github.com/dl805387" target="_blank"><p>Resume</p></a>
+            </div>
+            <div className="iconPlusLabel">
+                <a href="https://github.com/dl805387" target="_blank"><FontAwesomeIcon icon="envelope" size="2x" className="iconWhite"/></a>
+                <a href="https://github.com/dl805387" target="_blank"><p>Email</p></a>
+            </div>
         </div>
     );
 }
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,5 +2,4 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import {faLink, faAngleDoubleDown, faFile, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faGithub as fabFaGithub, faLinkedin as fabFaLinkedin} from '@fortawesome/free-brands-svg-icons'
 
-
 library.add(fabFaGithub, faLink, faAngleDoubleDown, faFile, faEnvelope, fabFaLinkedin);
